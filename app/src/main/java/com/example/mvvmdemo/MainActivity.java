8bf19ad88e1cd94ea71108d58039bf09448edb98
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-02-06 11:35:44 +0100

    Added showText() method and made the button call the method when clicked

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainText;
+    EditText mainEditText;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainText = findViewById(R.id.mainText);
+        mainEditText = findViewById(R.id.mainEditText);
+        mainButton = findViewById(R.id.mainButton);
+    }
+
+    public void showText(View view){
+        mainText.setText("clicked");
     }
 }
