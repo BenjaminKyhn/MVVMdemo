17a75bc3384bdf4563bedfd09914a4f98a472a4b
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-02-06 14:28:14 +0100

    Made the inputText an observable value and added an observer. When the button is pressed the observable inputText is set to whatever the editText view has inside it.

--- app/src/main/java/com/example/mvvmdemo/View/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/View/MainActivity.java
@@ -1,19 +1,28 @@
-package com.example.mvvmdemo;
+package com.example.mvvmdemo.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mvvmdemo.Model.ObservableText;
+import com.example.mvvmdemo.R;
+
+import java.util.Observable;
+import java.util.Observer;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView mainText;
+    public TextView mainText;
     EditText mainEditText;
     Button mainButton;
-    String textInput = "";
+
+    ObservableText ot = new ObservableText();
+    TextObserver to = new TextObserver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +32,21 @@ public class MainActivity extends AppCompatActivity {
         mainText = findViewById(R.id.mainText);
         mainEditText = findViewById(R.id.mainEditText);
         mainButton = findViewById(R.id.mainButton);
+
+        ot.addObserver(to);
     }
 
     public void showText(View view){
-        Data data = new Data();
-        data.setTextInput(mainEditText.getText().toString());
-        mainText.setText(data.getTextInput());
+        ot.setInputText(mainEditText.getText().toString());
+    }
+
+    public class TextObserver implements Observer{
+        @Override
+        public void update(Observable obs, Object obj){
+            Log.v("hej", "tommy");
+            mainText.setText(obj.toString());
+        }
     }
 }
+
+
