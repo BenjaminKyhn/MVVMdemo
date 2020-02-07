3383e9c0d4aca0e2b7265b99a46c21d9426740f4
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-02-07 09:46:38 +0100

    View now presents the model in uncapitalized form, but I'm unsure if I'm using wrapping.

--- app/src/main/java/com/example/mvvmdemo/Model/TextWrapper.java
+++ app/src/main/java/com/example/mvvmdemo/Model/TextWrapper.java
@@ -4,7 +4,7 @@ public class TextWrapper {
     public String uncapitalize(String string) {
         String uncappedString = "";
         for (int i = 0; i < string.length(); i++) {
-            if ((string.charAt(i) >= 'A') && (string.charAt(i) <= 'Z')) {
+            if (((string.charAt(i) >= 'A') && (string.charAt(i) <= 'Z')) || (string.charAt(i) == 'Æ')  || (string.charAt(i) == 'Ø')  || (string.charAt(i) == 'Å')) {
                 uncappedString += Character.toLowerCase(string.charAt(i));
             }
             else {
